209550786082618cac69816d064db354c6f3899b
Neophytos Michael <neom@sqreamtech.com>
2025-04-12 18:33:52 +0300

    More methods for bimaps.

--- src/main/scala/app/PersistentTreeBidiMap.scala
+++ src/main/scala/app/PersistentTreeBidiMap.scala
@@ -1,6 +1,6 @@
 package app
 
-import scala.collection.immutable.TreeMap
+import scala.collection.immutable.{TreeMap, TreeSet}
 
 final class PersistentTreeBidiMap[K: Ordering, V: Ordering] private (
     m0: TreeMap[K, V],
@@ -8,8 +8,13 @@ final class PersistentTreeBidiMap[K: Ordering, V: Ordering] private (
 ):
   def size: Int = m0.size
 
+  def isEmpty: Boolean = m0.isEmpty
+
   def getWithLeft(k: K): Option[V] = m0.get(k)
   def getWithRight(v: V): Option[K] = m1.get(v)
+
+  def getWithLeftOrElse(k: K, v: => V): V = m0.getOrElse(k, v)
+  def getWithRightOrElse(v: V, k: => K): K = m1.getOrElse(v, k)
 
   def updated(k: K, v: V): PersistentTreeBidiMap[K, V] =
     new PersistentTreeBidiMap[K, V](m0.removed(k).updated(k, v), m1.removed(v).updated(v, k))
@@ -27,12 +32,13 @@ final class PersistentTreeBidiMap[K: Ordering, V: Ordering] private (
   def iterator: Iterator[(K, V)] = m0.iterator
 
   def keysLeft: Iterable[K] = m0.keys
-
   def keysRight: Iterable[V] = m1.keys
 
   def containsLeftKey(k: K): Boolean = m0.contains(k)
-
   def containsRightKey(v: V): Boolean = m1.contains(v)
+
+  def keySetLeft: TreeSet[K] = m0.keySet
+  def keySetRight: TreeSet[V] = m1.keySet
 
 object PersistentTreeBidiMap:
   def empty[K: Ordering, V: Ordering] =
