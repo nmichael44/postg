d85853bd434ef885a7539a0b18496b2e35a8199a
Neophytos Michael <neom@sqreamtech.com>
2025-05-14 09:48:27 +0300

    Push missing files.

---
+++ src/test/scala/app/movieapptests/MovieAppTestUtils.scala
@@ -0,0 +1,27 @@
+package app.movieapptests
+
+import cats.data.NonEmptyVector
+import cats.effect.MonadCancelThrow
+
+import scala.util.control.NoStackTrace
+
+import app.services.MovieRepositoryService
+import app.MovieDbModel
+
+object MovieAppTestUtils:
+  val MovieRepositoryServiceNotImplemented: Exception =
+    new Exception("MovieRepositoryService not properly overridden in test") with NoStackTrace
+
+  class MovieRepositoryServiceShunning[F[_]: MonadCancelThrow as mc] extends MovieRepositoryService[F]:
+    override def getDirectorsDetails(firstName: Option[String], lastName: Option[String]): F[Seq[MovieDbModel.Director]] =
+      mc.raiseError(MovieRepositoryServiceNotImplemented)
+    override def getDirectorDetails(directorIds: NonEmptyVector[Long]): F[Map[Long, MovieDbModel.Director]] =
+      mc.raiseError(MovieRepositoryServiceNotImplemented)
+    override def getActorDetails(actorIds: NonEmptyVector[Long]): F[Map[Long, MovieDbModel.Actor]] =
+      mc.raiseError(MovieRepositoryServiceNotImplemented)
+    override def getMoviesByDirectorId(directorIds: NonEmptyVector[Long]): F[Map[Long, Seq[MovieDbModel.Movie]]] =
+      mc.raiseError(MovieRepositoryServiceNotImplemented)
+    override def getMovieDetails(movieIds: NonEmptyVector[Long]): F[Map[Long, MovieDbModel.Movie]] =
+      mc.raiseError(MovieRepositoryServiceNotImplemented)
+    override def createMovie(title: String, year: Int): F[Long] =
+      mc.raiseError(MovieRepositoryServiceNotImplemented)
---
+++ src/test/scala/app/movieapptests/ServiceStubs.scala
@@ -0,0 +1,34 @@
+package app.movieapptests
+
+import cats.effect.Async
+
+import app.services.{ExternalApiClientService, FileSystemService, ServerStateUpdateService}
+import org.http4s.Uri
+
+object ServiceStubs:
+  final class ExternalApiClientServiceStub[F[_]: Async as async] extends ExternalApiClientService[F]:
+    def fetchUri(uri: org.http4s.Uri): F[String] =
+      async.raiseError(new NotImplementedError(s"ExternalApiClientServiceStub.fetchUri($uri) not implemented for this test"))
+
+    override def fetchCompanyData(companyName: String): F[String] =
+      async.pure(s"Dummy company data for $companyName")
+
+    override def fetchAsJson[A](uri: Uri)(implicit decoder: io.circe.Decoder[A]): F[A] =
+      async.raiseError(
+        new NotImplementedError(s"ExternalApiClientServiceStub.fetchAsJson($uri) not implemented for this test"),
+      )
+
+  final class FileSystemServiceStub[F[_]: Async as async] extends FileSystemService[F]:
+    override def readFileContent(fileName: String): F[String] =
+      async.pure(s"Dummy file content for $fileName")
+
+    override def readTwoFilesInParallel(fileName1: String, fileName2: String): F[String] =
+      async.pure(s"Dummy parallel file content for $fileName1 & $fileName2")
+
+  final class ServerStateUpdateServiceStub[F[_]: Async as async] extends ServerStateUpdateService[F]:
+    override def get(movieId: Long): F[Option[Int]] = ???
+
+    override def getAllCounts: F[Map[Long, Int]] = ???
+
+    override def incrementAndGet(movieId: Long): F[Int] =
+      async.pure(1) // Return a dummy count
