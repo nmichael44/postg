e2a9ca683a7a11e6a8322db9e7628ad4b551d253
Neophytos Michael <neom@sqreamtech.com>
2025-07-24 12:39:12 +0300

    Use the  syntax.

--- src/main/scala/app/ImplicitConversions.scala
+++ src/main/scala/app/ImplicitConversions.scala
@@ -7,29 +7,46 @@ import cats.syntax.functor.*
 import scala.collection.View
 
 object ImplicitConversions:
-  extension [A](nev: NonEmptyVector[A]) inline def view: View[A] = nev.toVector.view
+  extension [A](nev: NonEmptyVector[A])
+    inline def view: View[A] =
+      nev.toVector.view
+    end view
 
   extension [F[_], G[_]: Functor, O](s: fs2.Stream.CompileOps[F, G, O])
     inline def theLast(using fs2.Compiler[F, G]): G[O] =
       s.last.map(_.get)
+    end theLast
 
   extension [F[_]: Applicative as app, A](b: Boolean)
     inline def whenA(fa: F[A]): F[Unit] =
       import cats.syntax.all.*
       fa.whenA(b)(using app)
+    end whenA
 
   extension (obj: Any)
     inline def safeAs[C]: Option[C] = obj match {
       case c: C => Some(c)
       case _ => None
     }
+    end safeAs
 
-  extension [F[_]: Functor, A](fa: F[A]) inline def lifte[B]: EitherT[F, B, A] = EitherT.liftF[F, B, A](fa)
+  extension [F[_]: Functor, A](fa: F[A])
+    inline def lifte[B]: EitherT[F, B, A] =
+      EitherT.liftF[F, B, A](fa)
+    end lifte
 
-  extension [F[_], A, B](fe: F[Either[A, B]]) inline def toEitherT: EitherT[F, A, B] = EitherT(fe)
+  extension [F[_], A, B](fe: F[Either[A, B]])
+    inline def toEitherT: EitherT[F, A, B] =
+      EitherT(fe)
+    end toEitherT
 
-  extension [F[_]: Applicative, A, B](e: Either[A, B]) inline def toEitherT: EitherT[F, A, B] = EitherT.fromEither(e)
+  extension [F[_]: Applicative, A, B](e: Either[A, B])
+    inline def toEitherT: EitherT[F, A, B] =
+      EitherT.fromEither(e)
+    end toEitherT
 
   extension [F[_]: Functor, A](o: F[Option[A]])
-    inline def toEitherT[B](ifNone: => B): EitherT[F, B, A] = EitherT.fromOptionF(o, ifNone)
+    inline def toEitherT[B](ifNone: => B): EitherT[F, B, A] =
+      EitherT.fromOptionF(o, ifNone)
+    end toEitherT
 end ImplicitConversions
